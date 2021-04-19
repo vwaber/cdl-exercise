36bcf75a05a22378ba74fd33d8a617363565e220
Vaan Waber <vwaber@gmail.com>
2021-04-19 12:53:50 -0700

    Enable and add view binding
    This also removes some unused imports.

--- app/src/main/java/com/vwaber/cdlexercise/MainActivity.kt
+++ app/src/main/java/com/vwaber/cdlexercise/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.vwaber.cdlexercise
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import com.vwaber.cdlexercise.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
