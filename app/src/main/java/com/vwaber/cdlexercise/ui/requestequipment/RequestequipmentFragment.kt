aa2eceac50d66e12f49755292e37b156aa9a2887
Vaan Waber <vwaber@gmail.com>
2021-04-20 21:33:58 -0700

    Remove unused class

--- app/src/main/java/com/vwaber/cdlexercise/ui/requestequipment/RequestequipmentFragment.kt
+++ app/src/main/java/com/vwaber/cdlexercise/ui/requestequipment/RequestequipmentFragment.kt
@@ -5,28 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.vwaber.cdlexercise.databinding.FragmentRequestequipmentBinding
 
 class RequestequipmentFragment : Fragment() {
-
-    private lateinit var binding: FragmentRequestequipmentBinding
-    private lateinit var requestequipmentViewModel: RequestequipmentViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentRequestequipmentBinding.inflate(layoutInflater)
-        requestequipmentViewModel =
-            ViewModelProvider(this).get(RequestequipmentViewModel::class.java)
-
-        requestequipmentViewModel.text.observe(viewLifecycleOwner, Observer {
-//            binding.textHome.text = it
-        })
+        val binding = FragmentRequestequipmentBinding.inflate(layoutInflater)
 
         binding.ivCdl.setOnClickListener {
             findNavController().navigate(
