d8e1f336ef00cdf6560b53591933144f64cbc364
Vaan Waber <vwaber@gmail.com>
2021-04-21 03:41:21 -0700

    Update formatting

--- app/src/main/java/com/vwaber/cdlexercise/MainViewModel.kt
+++ app/src/main/java/com/vwaber/cdlexercise/MainViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel(){
+class MainViewModel : ViewModel() {
 
     data class ViewState(
         val destination: Destination = Destination.REQUEST_EQUIPMENT
